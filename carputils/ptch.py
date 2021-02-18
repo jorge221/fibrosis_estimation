e635daf1673a450bbbfec6edd940c7d75cc484e2
Jorge Sanchez <jorge.arciniegas@kit.edu>
2021-02-18 09:50:24 +0100

    Updated file

--- carputils/ptch.py
+++ carputils/ptch.py
@@ -26,10 +26,10 @@ def parser():
 
 def jobID(args):
     #================================
-    #Generate name of top level output directory.
+    #Generate name of top level output directory
     #================================
 
-    return 'patch_{}_{}_ms'.format(args.catheter, args.IM) 
+    return 'patch_{}_{}'.format(args.catheter, args.IM) 
 
 
 @tools.carpexample(parser, jobID) 
@@ -96,18 +96,22 @@ def run(args, job):
                  '-gregion[4].g_bath', 1E-6,      #Catheter insulator
                  '-gregion[4].num_IDs', 1,
                  '-gregion[4].ID', 200]
+
     if args.catheter=='HDGRID':
-        electrodes = []
+        electrodes = [202,206,210,214,222,226,230,234,242,246,250,254,262,266,270,274]
     elif args.catheter ='LASSO':
-        electrodes = []
+        electrodes = [204,207,214,217,224,227,234,237,244,247,254,257,264,267,274,277,284,287,294,297]
     else:
         print('Catheter not yet implemented')
-    
-    g_vec = g_vec + electrodes
 
+    g_elec = ['-gregion[5].g_bath', 1E6,      #Electrodes
+              '-gregion[5].num_IDs', len(electrodes),
+              '-gregion[5].ID', electrode]
+        
+    g_reg = g_reg + g_elec
 
     #================================
-    #Number4: Defining the Stimulus:
+    #Number4: Defining the Stimulus
     #================================
 
     stim = ['-num_stim', 1,                               
@@ -116,55 +120,66 @@ def run(args, job):
             '-stimulus[0].duration', 2.0,       #Duration of the stimulus in ms
             '-stimulus[0].start', 0,             #Start of the stimulation in ms
             '-stimulus[0].bcl', 600,             #Basic Cycle Length in ms
-            '-stimulus[0].npls', 5,              #Number of pulses 
-            '-stimulus[0].x0', 0,
+            '-stimulus[0].npls', 5]              #Number of pulses 
+    
+    if args.location == 'left':
+        sitm_reg = ['-stimulus[0].x0', 0,
             '-stimulus[0].xd', 310,
             '-stimulus[0].y0', 0,
             '-stimulus[0].yd', 50000,
             '-stimulus[0].z0', 1000,
-            '-stimulus[0].zd', 2000,
-            '-floating_ground',    1 ]
+            '-stimulus[0].zd', 2000 ]
+    elif args.location=='bottom':
+        sitm_reg = ['-stimulus[0].x0', 0,
+                    '-stimulus[0].xd', 50000,
+                    '-stimulus[0].y0', 0,
+                    '-stimulus[0].yd', 310,
+                    '-stimulus[0].z0', 1000,
+                    '-stimulus[0].zd', 2000 ]
+    elif args.locatio=='leftcorner':
+        sitm_reg = ['-stimulus[0].x0', 29700,
+                    '-stimulus[0].xd', 300,
+                    '-stimulus[0].y0', 29700,
+                    '-stimulus[0].yd', 300,
+                    '-stimulus[0].z0', 1000,
+                    '-stimulus[0].zd', 2000 ]
+    else:
+        print('Location not yet implemented')
 
+    stim = stim + stim_reg
 
     #============================================
-    #Number5: Defining the Mathmatical Solvers:
+    #Number5: Defining the Mathmatical Solvers
     #============================================
 
     # NUMERICAL PARAMETERS
     num_par = ['-dt', 10,        #usec 
-               '-bidomain', 2]
+               '-bidomain', 2,
+               '-mass_lumping', 0]
 
     # I/O
     IO_par = ['-spacedt', 0.5,
-              '-timedt',  10.0]
+              '-timedt',  10.0,
+              '-gridout_i', 3]  #Outputs the intracellular grid
 
     #=====================================
-    #Number6: Output everything to openCARP:
+    #Number6: Output everything to openCARP
     #=====================================
 
-    cmd = tools.carp_cmd()      #initialises cmd as a class that accepts valid opencarp inputs
+    cmd = tools.carp_cmd()      #Initialises cmd as a class that accepts valid opencarp inputs
 
-    if args.catheter=='HDGRID':
-        cmd += tools.gen_physics_opts(ExtraTags=[100,203,206,209,212,223,226,229,232,243,246,249,252,263,266,269,272,200,201,202,204,205,207,208,210,211,213,214,215,216,221,222,224,225,227,228,230,231,233,234,235,236,241,242,244,245,247,248,250,251,253,254,255,256,261,262,264,265,267,268,270,271,273,274,275,276], IntraTags=[1,2])
-    elif args.catheter ='LASSO':
-        cmd += tools.gen_physics_opts(ExtraTags=[100,203,206,209,212,223,226,229,232,243,246,249,252,263,266,269,272,200,201,202,204,205,207,208,210,211,213,214,215,216,221,222,224,225,227,228,230,231,233,234,235,236,241,242,244,245,247,248,250,251,253,254,255,256,261,262,264,265,267,268,270,271,273,274,275,276], IntraTags=[1,2])
-    else:
-        print('Catheter not yet implemented')
+    cmd += tools.gen_physics_opts(ExtraTags=[50,100,200] + electrodes, IntraTags=[1,2]) #Define physical regions
 
-
-    cmd += imp_reg              #ionic model defined earlier
-    cmd += g_reg                #conductivities of the regions we defined
-    cmd += stim                 #stim definition based on coordinates
-    cmd += num_par              #numerical solver parameters
+    cmd += imp_reg              #Ionic model defined earlier
+    cmd += g_reg                #Conductivities of the regions we defined
+    cmd += stim                 #Stim definition based on coordinates
+    cmd += num_par              #Numerical solver parameters
     cmd += IO_par               #Output parameters
     simID = job.ID              #Name of our simulation
-
     
-    cmd += ['-meshname', args.catheter,
-            '-mass_lumping', 0,
-            '-tend',     args.duration,
-            '-gridout_i', 3,
-            '-simID',    simID ]
+    cmd += ['-meshname', meshname,
+            '-tend', args.duration,
+            '-simID', simID ]
 
     # Run simulation 
     job.carp(cmd)
